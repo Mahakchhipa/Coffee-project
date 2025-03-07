b4b4f502075ecd717048f96c611b6d6771f42c97
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-03-07 16:03:04 +0530

    project setup

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-center font-bold text-4xl w-full content-center h-full">
+      <main> Coffee Shop </main>
+    </div>
+  );
+}
